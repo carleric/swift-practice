23627b95bb379944e8acf6410cca164c35e12006
Carl Bolstad <cbolstad@gmail.com>
2016-02-11 13:42:30 -0800

    a few more simple problems with numbers

--- swift-practice.playground/Pages/Numbers.xcplaygroundpage/Contents.swift
+++ swift-practice.playground/Pages/Numbers.xcplaygroundpage/Contents.swift
@@ -47,4 +47,37 @@ countOfRangeMistakesInArray([0, 1, 2, 3, 4, 5, 7], 1, 7) == 2
 countOfRangeMistakesInArray([0, 1, 2, 2, 3, 4, 5, 7], 1, 7) == 3
 
 
+
+/**
+ * factorial, recursive
+ */
+func factorial(integer:Int) -> Int {
+   if(integer == 1) { return 1 }
+   else {
+      return integer * factorial(integer - 1)
+   }
+}
+factorial(1) == 1
+factorial(2) == 2
+factorial(3) ==  6
+factorial(4) ==  24
+factorial(5) ==  120
+
+/**
+ * remove all duplicates from an array
+ */
+func removeAllDuplicates(inputArray:Array<Int>) -> Array<Int> {
+   print(inputArray)
+   var found:Set<Int> = Set()
+   for num in inputArray {
+      if(found.contains(num) == false) {
+         found.insert(num)
+      }
+   }
+   return Array(found.sort())
+}
+
+var removed = removeAllDuplicates([2, 3, 2, 4, 6, 6, 1])
+removed == [1, 2, 3, 4, 6]
+
 //: [Next](@next)
